65e37a19f52b73c7e81860a31de242a0ea05ac10
Antonio Piras <antonio.piras90@gmail.com>
2023-06-29 15:04:08 +0200

    js-pwdgen-wannabe

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+// Chiedo all’utente il suo nome,
+const userName = prompt("Inserisci il tuo nome")
+
+
+
+// chiedo il suo cognome,
+const userSurname = prompt("Inserisci il tuo cognome")
+
+
+
+// chiedo il suo colore preferito
+const favColor = prompt("Inserisci il tuo colore preferito")
+
+
+
+// // Infine scrivi sulla pagina
+document.getElementById("concatenazione").innerHTML = userName + userSurname +favColor + "21"
