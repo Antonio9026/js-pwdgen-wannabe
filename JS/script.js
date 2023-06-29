0c9bd9191caeb75971716d294651b193d8b1abcd
Antonio Piras <antonio.piras90@gmail.com>
2023-06-29 15:51:50 +0200

    bonus somma età

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,22 @@
 // Chiedo all’utente il suo nome,
-const userName = prompt("Inserisci il tuo nome")
+const userName = prompt("Inserisci il tuo nome");
 
 
 
 // chiedo il suo cognome,
-const userSurname = prompt("Inserisci il tuo cognome")
+const userSurname = prompt("Inserisci il tuo cognome");
 
 
 
 // chiedo il suo colore preferito
-const favColor = prompt("Inserisci il tuo colore preferito")
+const favColor = prompt("Inserisci il tuo colore preferito");
+
+// chiedo la sua età
+const age = parseInt(prompt("Inserisci la tua età"));
 
 
+const num = 21;
+const sumAge = age + num;
 
 // // Infine scrivi sulla pagina
-document.getElementById("concatenazione").innerHTML = userName + userSurname +favColor + "21"
+document.getElementById("concatenazione").innerHTML = userName + userSurname +favColor + sumAge;
